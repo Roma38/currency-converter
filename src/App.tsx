a95dd9a5e13e44af1fba04d4bc90853f24b264a3
Alex K <agent150891@gmail.com>
2019-04-10 18:05:10 +0300

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Form, Container, Button, Message, Icon } from 'semantic-ui-react'
+import { API_HOST, API_KEY, CURRENCIES } from "./config.js"
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    amount: 0,
+    fromCurrency: "",
+    toCurrency: "",
+    requestState: "",
+    result: null
+  };
+
+  handleChange = (_event: any, data: any) => {
+    const { value, name } = data;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = () => {
+    const { amount, fromCurrency, toCurrency } = this.state;
+    this.setState({ requestState: "loading" });
+    axios.get(`${API_HOST}/1.0.3/convert?from=${fromCurrency}&to=${toCurrency}&quantity=${amount}&api_key=${API_KEY}`)
+      .then(({ data }) => {
+        data.error ?
+          this.setState({ requestState: "failure", result: data.message }) :
+          this.setState({ requestState: "succeed", result: data.text });
+      })
+      .catch(() => this.setState({ requestState: "failure", result: "Oops... something went wrong :(" }));
+  };
+
+  swapCurrencies = () => this.setState({ fromCurrency: this.state.toCurrency, toCurrency: this.state.fromCurrency });
+
   render() {
+    const { amount, fromCurrency, toCurrency, requestState, result } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Container>
+          <Form onSubmit={this.handleSubmit} size="large">
+            <Form.Group widths='equal'>
+              <Form.Input fluid label='Amount' placeholder='0' type='number' min='1' required name="amount" value={amount} onChange={this.handleChange} />
+              <Form.Select fluid label='Convert' options={CURRENCIES} placeholder='Currency' required name="fromCurrency" value={fromCurrency} onChange={this.handleChange} />
+              <Button type="button" icon='exchange' className="swap-button" circular onClick={this.swapCurrencies} />
+              <Form.Select fluid label='Into' options={CURRENCIES} placeholder='Currency' required name="toCurrency" value={toCurrency} onChange={this.handleChange} />
+            </Form.Group>
+            <Form.Button>Convert</Form.Button>
+          </Form>
+
+          {requestState === "loading" && <Message icon>
+            <Icon name='circle notched' loading />
+            Loading...
+          </Message>}
+          {requestState === "succeed" && <Message icon positive>
+            <Icon name='thumbs up outline' />
+            {result}
+          </Message>}
+          {requestState === "failure" && <Message icon negative>
+            <Icon name='frown outline' />
+            {result}
+          </Message>}
+        </Container>
       </div>
     );
   }
---
+++ src/config.js
@@ -0,0 +1,14 @@
+export const API_HOST = "https://forex.1forge.com";
+export const API_KEY = "yWftxwT3UnBDJ0EVTp0E2ZKssFkM8jwF";
+export const CURRENCIES = [
+  { key: 'USD', text: 'US dollar (USD)', value: 'USD' },
+  { key: 'EUR', text: 'Euro (EUR)', value: 'EUR' },
+  { key: 'JPY', text: 'Japanese yen (JPY)', value: 'JPY' },
+  { key: 'GBP', text: 'Pound sterling (GBP)', value: 'GBP' },
+  { key: 'AUD', text: 'Australian dollar (AUD)', value: 'AUD' },
+  { key: 'CAD', text: 'Canadian dollar (CAD)', value: 'CAD' },
+  { key: 'CHF', text: 'Swiss franc (CHF)', value: 'CHF' },
+  { key: 'CNH', text: 'Chinese renminbi (CNH)', value: 'CNH' },
+  { key: 'SEK', text: 'Swedish krona (SEK)', value: 'SEK' },
+  { key: 'NZD', text: 'New Zealand dollar (NZD)', value: 'NZD' },
+];
