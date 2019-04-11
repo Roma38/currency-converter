e71a088b73b69728c2d869d0c8c4bba050b00455
Alex K <agent150891@gmail.com>
2019-04-11 14:09:58 +0300

    another api and interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Form, Container, Button, Message, Icon } from 'semantic-ui-react'
-import { API_HOST, API_KEY, CURRENCIES } from "./config.js"
+import { Form, Container, Button, Message, Icon, Grid } from 'semantic-ui-react'
+import { API_ENDPOINT_OANDA, API_KEY_OANDA, SELECT_OPTIONS } from "./config.js"
 import './App.css';
 import axios from "axios";
 
 class App extends Component {
   state = {
-    amount: 0,
+    amount: 1,
     fromCurrency: "",
     toCurrency: "",
     requestState: "",
@@ -21,11 +21,12 @@ class App extends Component {
   handleSubmit = () => {
     const { amount, fromCurrency, toCurrency } = this.state;
     this.setState({ requestState: "loading" });
-    axios.get(`${API_HOST}/1.0.3/convert?from=${fromCurrency}&to=${toCurrency}&quantity=${amount}&api_key=${API_KEY}`)
+    axios.get(`${API_ENDPOINT_OANDA}?api_key=${API_KEY_OANDA}&base=${fromCurrency}&quote=${toCurrency}`)
       .then(({ data }) => {
-        data.error ?
+        console.log(data);
+        /* data.error ?
           this.setState({ requestState: "failure", result: data.message }) :
-          this.setState({ requestState: "succeed", result: data.text });
+          this.setState({ requestState: "succeed", result: data.text }); */
       })
       .catch(() => this.setState({ requestState: "failure", result: "Oops... something went wrong :(" }));
   };
@@ -34,31 +35,52 @@ class App extends Component {
 
   render() {
     const { amount, fromCurrency, toCurrency, requestState, result } = this.state;
+
     return (
       <div className="App">
         <Container>
           <Form onSubmit={this.handleSubmit} size="large">
+            <Grid divided='vertically'>
+              <Grid.Row columns={3}>
+                <Grid.Column width={7} >
+                  <Form.Select search fluid label='Convert' options={SELECT_OPTIONS} placeholder='Currency' name="fromCurrency" value={fromCurrency} onChange={this.handleChange} />
+                </Grid.Column>
+                <Grid.Column width={2}>
+                  <Button type="button" icon='exchange' className="swap-button" circular onClick={this.swapCurrencies} />
+                </Grid.Column>
+                <Grid.Column width={7} >
+                  <Form.Select search fluid label='Into' options={SELECT_OPTIONS} placeholder='Currency' name="toCurrency" value={toCurrency} onChange={this.handleChange} />
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  <Form.Input fluid type='number' min='1' required name="amount" value={amount} onChange={this.handleChange} />
+                </Grid.Column>
+                <Grid.Column>
+                  {requestState === "loading" && <Message icon>
+                    <Icon name='circle notched' loading />
+                    Loading...
+                  </Message>}
+                  {requestState === "succeed" && <Message icon positive>
+                    <Icon name='thumbs up outline' />
+                    {result}
+                  </Message>}
+                  {requestState === "failure" && <Message icon negative>
+                    <Icon name='frown outline' />
+                    {result}
+                  </Message>}
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
             <Form.Group widths='equal'>
-              <Form.Input fluid label='Amount' placeholder='0' type='number' min='1' required name="amount" value={amount} onChange={this.handleChange} />
-              <Form.Select fluid label='Convert' options={CURRENCIES} placeholder='Currency' required name="fromCurrency" value={fromCurrency} onChange={this.handleChange} />
-              <Button type="button" icon='exchange' className="swap-button" circular onClick={this.swapCurrencies} />
-              <Form.Select fluid label='Into' options={CURRENCIES} placeholder='Currency' required name="toCurrency" value={toCurrency} onChange={this.handleChange} />
+
+
             </Form.Group>
             <Form.Button>Convert</Form.Button>
           </Form>
 
-          {requestState === "loading" && <Message icon>
-            <Icon name='circle notched' loading />
-            Loading...
-          </Message>}
-          {requestState === "succeed" && <Message icon positive>
-            <Icon name='thumbs up outline' />
-            {result}
-          </Message>}
-          {requestState === "failure" && <Message icon negative>
-            <Icon name='frown outline' />
-            {result}
-          </Message>}
+
         </Container>
       </div>
     );
